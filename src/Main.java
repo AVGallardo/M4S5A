5aafcd283761287512eb43ab183776d32cedd5c6
Lana <lana@lana.com>
2024-08-28 20:51:38 -0400

    tarea de arreglos, asignaturas, notas y promedio

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import calculo_promedio.Calculo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Calculo c = new Calculo();
+        c.solicitar();
+    }
+}
---
+++ src/calculo_promedio/Calculo.java
@@ -0,0 +1,30 @@
+package calculo_promedio;
+
+import java.util.Scanner;
+
+public class Calculo {
+    public void solicitar(){
+
+        Scanner sc = new Scanner(System.in);
+        String[] listaMaterias = new String[5];
+        double[] listaNotas = new double[5];
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Ingrese el NOMBRE de la asignatura");
+            listaMaterias[i] = sc.next();
+            System.out.println("Ingrese el PROMEDIO de la asignatura: "+listaMaterias[i]);
+            listaNotas[i] = sc.nextDouble();
+        }
+        mostrar(listaMaterias, listaNotas);
+    }
+    public void mostrar(String[] listaMaterias, double[] listaNotas){
+        double notas = 0;
+
+        System.out.println("Sus notas son las siguientes");
+        for(int i = 0; i < 5; i++){
+            System.out.println( listaMaterias[i] + " = " + listaNotas[i]);
+            notas = notas + listaNotas[i];
+        }
+        System.out.println("Su PROMEDIO GENERAL es de = " + (notas/5));
+    }
+}
